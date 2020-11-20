dfa29d69661ec3dbe9170f4cb303a665f15ef8f2
wesleysrael <wesley12israel@gmail.com>
2020-11-20 13:06:12 -0300

    Adicionado função para verificar input vazio

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function validate(e) {
   var emailAddress = document.querySelector("#email").value;
-  console.log(emailAddress);
   var emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
 
   if (!emailFilter.test(emailAddress)) {
     alert("E-mail inválido");
     return false;
   }
-
   alert(`Bem-vindo ${emailAddress}!`);
+  return true;
+}
+
+function IsEmpty() {
+  if (document.forms["validateEmail"].email.value === "") {
+    console.log("chegou carai!");
+    alert("Preencha o campo com o seu email!");
+    return false;
+  }
   return true;
 }
 
